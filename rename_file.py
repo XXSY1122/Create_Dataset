aba06a9f8eeac7cf82720b0b9f88b58c2d42e6c4
PARK SEO YOUNG <99531247+XXSY1122@users.noreply.github.com>
2023-11-13 18:11:48 +0900

    Update rename_file.py

--- rename_file.py
+++ rename_file.py
@@ -1,33 +1,62 @@
+from glob import glob
+from PIL import Image
 import os
+import re
+import matplotlib.pyplot as plt
+import cv2
+import shutil
 
-# 전체 파일명을 숫자로 변경하는 프로그램
-# 이미지 파일 이름이 뒤죽박죽 상태일때 숫자 1.2.3.4.5...순으로 전체 변경해준다. 
+# 파일 이름 변경
+# 이미지와 TXT의 변형없이 개별적으로 숫자별로 정렬하여 SORT
 
-# 이미지 파일 경로
-path = "/Users/sy/Desktop/Create_Dataset/dd"
+n=2476 # 원하는 첫번째 파일명 (ex. 2476이면 2476.png / 2476.txt 이 시작파일)
 
-files = os.listdir(path)
-files.sort()
+def extract_number(filename):
+    numbers = re.findall(r'\d+', filename)
+    return int(numbers[0]) if numbers else None
 
-# Step 1: Rename files to temporary names
-for idx, file in enumerate(files):
-    old_file_path = os.path.join(path, file)
-    # Construct the new file name with a temporary prefix
-    new_file_name = f"tmp_{idx + 1}.png"
-    new_file_path = os.path.join(path, new_file_name)
-    os.rename(old_file_path, new_file_path)
+#JAWSBAR
+#p1=f'C:\\Users\\ksl97\\Downloads\\nugabar\\NUGABAR2\\*.png'
+p1=f'C:\\Users\\ksl97\\Downloads\\ok\\OKDONGJA2\\*.png'
+p2=f'C:\\Users\\ksl97\\Downloads\\ok\\OKDONGJA2\\*.txt'
 
-# Get a list of all temporary files in the directory
-tmp_files = [file for file in os.listdir(path) if file.startswith("tmp_")]
+img_list = glob(p1)
+label_list = glob(p2)
 
-# Sort temporary files by name
-tmp_files.sort()
+img_list.sort(key=extract_number)
+label_list.sort(key=extract_number)
 
-# Step 2: Rename temporary files to final names
-for idx, file in enumerate(tmp_files):
-    old_file_path = os.path.join(path, file)
-    # Construct the final file name without the temporary prefix
-    new_file_name = f"{idx + 1}.png"
-    new_file_path = os.path.join(path, new_file_name)
-    os.rename(old_file_path, new_file_path)
+label_name_list=[]
 
+for a in label_list:
+    base_name = os.path.basename(a)
+    file_name, _ = os.path.splitext(base_name)
+    label_name_list.append(file_name)
+
+to_remove = []
+
+for d in img_list:
+    base_name = os.path.basename(d)
+    file_name, _ = os.path.splitext(base_name)
+    if file_name not in label_name_list:
+        to_remove.append(d)
+
+for item in to_remove:
+    img_list.remove(item)
+
+for i in range(len(img_list)):
+
+    new_img_path=f"C:\\Users\\ksl97\\Downloads\\images\\{n}.png"
+    new_label_path=f"C:\\Users\\ksl97\\Downloads\\labels\\{n}.txt"
+
+    img=cv2.imread(img_list[i])
+    if img is None:
+        print(f"Failed to read image: {img_list[i]}")
+        continue
+
+    cv2.imwrite(new_img_path,img)
+
+    shutil.copy(label_list[i], new_label_path)
+    n+=1
+
+print(len(img_list), len(label_list), n,end=' ')
