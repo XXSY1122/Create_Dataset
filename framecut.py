895bdcb8f1be5548beb3620202c9936af0c70828
PARK SEO YOUNG <99531247+XXSY1122@users.noreply.github.com>
2024-01-31 16:14:14 +0900

    Update framecut.py

--- framecut.py
+++ framecut.py
@@ -3,7 +3,7 @@ import os
 
 # 동영상 프레임 추출 코드 
 
-filepath = "C:\\Users\\ksl97\\Downloads\\IMG_6308.mp4"    #동영상 경로
+filepath = "C:\\Users\\user\\Downloads\\IMG_6308.mp4"    #동영상 경로
 video = cv2.VideoCapture(filepath)
 frame_count=int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 #print(frame_count)
@@ -12,6 +12,6 @@ for i in range(frame_count):
     ret, frame=video.read()
     if ret and i%13==0:
         n+=1
-        image_path=f"C:\\Users\\ksl97\\Downloads\\chk\\{n}.png"  #저장 경로
+        image_path=f"C:\\Users\\user\\Downloads\\chk\\{n}.png"  #저장 경로
         cv2.imwrite(image_path, frame)
 video.release()
